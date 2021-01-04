4a6d970c66b8bce0031fd4a3b152da071cf144c1
Juan Rolo <juanluis211@outlook.com>
2021-01-04 23:59:43 +0100

    Contador

--- holamundo/js/12-ejercicio2.js
+++ holamundo/js/12-ejercicio2.js
@@ -1,1 +1,21 @@
 'use strict'
+/**
+ * Utilizando un bucle, mostrar la suma y la media de los numeros introducidos, hasta introducir un numero negativo y ahi mostrar el resultado.
+ */
+
+var suma = 0;
+var contador = 0;
+
+do{
+  var numero = parseInt(prompt('Introduce numeros hasta que metas uno negativo', 0));
+
+  if (isNaN(numero)){
+    numero = 0;
+  } else if( numero >= 0){
+    suma += numero;
+
+    contador++;
+  }
+  console.log(suma, contador);
+
+} while (numero >= 0);
