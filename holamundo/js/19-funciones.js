4e2efc51b2b9fa8863d220c595f5da345e7fb986
Juan Rolo <juanluis211@outlook.com>
2021-01-13 00:04:14 +0100

    Funciones

---
+++ holamundo/js/19-funciones.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Funciones
+// Una funcion es una agrupacion reutilizable de un conjunto de intrucciones que podemos utilizar tantas veces como queramos.
+
+function calculadora(){
+  console.log("Hola soy la calculadora");
+  console.log("Si soy yo");
+
+  return "Aqui veo lo que devuelve al funcion";
+}
+
+calculadora();
