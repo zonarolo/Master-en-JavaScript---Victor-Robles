09d34f1648667d352d365e1cae6a1f1f3c3f80c7
Juan Rolo <juanluis211@outlook.com>
2021-01-13 00:40:05 +0100

    Parametros opcionales

--- holamundo/js/19-funciones.js
+++ holamundo/js/19-funciones.js
@@ -3,13 +3,16 @@
 // Funciones
 // Una funcion es una agrupacion reutilizable de un conjunto de intrucciones que podemos utilizar tantas veces como queramos.
 
-function calculadora(num1, num2){
-  var calculadora = `Suma: ${num1+num2}               <br/> \n Resta: ${num1-num2}                  <br/> \n Multiplicacion: ${num1*num2}            <br/> \n Division: ${num1/num2}`;
+function calculadora(num1, num2, mostrar = false){
 
+  var calculadora;
+  mostrar === true ? calculadora = `Suma: ${num1+num2}               <br/> \n Resta: ${num1-num2}                  <br/> \n Multiplicacion: ${num1*num2}            <br/> \n Division: ${num1/num2}` : 'Debe ser true';
+
+  console.log(mostrar);
   return calculadora; 
 }
 
 var num1 = parseInt(prompt('Introduce el primer numero',0));
 var num2 = parseInt(prompt('Introduce el segundo numero',0));
-console.log(calculadora(num1, num2));
-document.write(calculadora(num1,num2));
+console.log(calculadora(num1, num2, false));
+document.write(calculadora(num1,num2, true));
